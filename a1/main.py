d364d3b18e536621b6c4f819fc832a26b9f20d80
hrshAnand <anand.2@iitj.ac.in>
2022-01-19 22:36:15 +0530

    adds code to encrypt and decrypt

---
+++ a1/main.py
@@ -0,0 +1,45 @@
+def validate(message, key):
+    if len(message) != len(key):
+        print("Error: message and key must be the same length")
+        exit()
+    for x in range(len(message)):
+        if ord(message[x]) < ord('A') or ord(message[x]) > ord('Z'):
+            print("Error: message must be upper case")
+            exit()
+        if ord(key[x]) < ord('A') or ord(key[x]) > ord('Z'):
+            print("Error: key must be upper case")
+            exit()
+
+
+def encrypt(message, key):
+    validate(message, key)
+
+    ciphertext = ""
+    for i in range(len(message)):
+        sume = ord(message[i]) + ord(key[i]) - ord('A')
+        if sume > 26 + ord('A'):
+            ciphertext += chr(sume - 26)
+        else:
+            ciphertext += chr(sume)
+    return ciphertext
+
+
+def decrypt(ciphertext, key):
+    validate(ciphertext, key)
+
+    message = ""
+    for i in range(len(ciphertext)):
+        diff = ord(ciphertext[i]) - ord(key[i])
+        if diff < 0:
+            message += chr(diff + 26 + ord('A'))
+        else:
+            message += chr(diff + ord('A'))
+    return message
+
+
+key = "VERYSECRET"
+message = "IITJODHPUR"
+
+cipher = encrypt(message, key)
+print("Cipher text:", cipher)
+print("Decrypted Message: ", decrypt(cipher, key))
